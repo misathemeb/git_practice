c5814536ec167509645020f80e67f150c894d886
misathemeb <melissabridi@gmail.com>
2022-07-21 14:28:26 -0400

    created Model class

--- python-REST-API/test.py
+++ python-REST-API/test.py
@@ -3,21 +3,18 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-data =  [{"likes": 10,  "name": 'melissa', "views": 1000000}, 
-         {"likes": 3,  "name": 'how to make a REST api', "views": 508},
-         {"likes": 100, "name": 'oliver catches fish on cat tv', "views": 7000000}]
+# data =  [{"likes": 10,  "name": 'melissa', "views": 1000000}, 
+#          {"likes": 3,  "name": 'how to make a REST api', "views": 508},
+#          {"likes": 100, "name": 'oliver catches fish on cat tv', "views": 7000000}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + 'video/' + str(i), data[i])
-    print(response.json())
+# for i in range(len(data)):
+#     response = requests.put(BASE + 'video/' + str(i), data[i])
+#     print(response.json())
 
-input()  
-response = requests.delete(BASE + "video/0")
-print(response)
 
 
 input()
-response = requests.get(BASE + 'video/2')
+response = requests.patch(BASE + 'video/2', {"views": 99})
 print(response.json())
 
 
